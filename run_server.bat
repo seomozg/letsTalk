526ccf403ef83a2d7a5b0ee34a4727bdabd615c2
seomozg <81183628+seomozg@users.noreply.github.com>
2025-12-12 15:24:05 +0700

    Update run_server.bat

--- run_server.bat
+++ run_server.bat
@@ -2,7 +2,7 @@
 echo Setting GEMINI_API_KEY environment variable...
 
 REM Replace YOUR_API_KEY_HERE with your actual Gemini API key
-SET GEMINI_API_KEY=AIzaSyB2OzJ452gDO65Wa7Z4JwygJDyfBY9wLek
+SET GEMINI_API_KEY=
 
 REM Or use environment variable if already set
 REM set GEMINI_API_KEY=%GEMINI_API_KEY%
